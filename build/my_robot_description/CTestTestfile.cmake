824434959d9632678dbb7fb8eb508cf5f1cbe4fa
Safrao Santos <santos_enoque@cyberdyne.jp>
2023-09-27 10:58:29 +0000

    robot description

---
+++ build/my_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /ros2_autonomous_robot/src/my_robot_description
+# Build directory: /ros2_autonomous_robot/build/my_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
